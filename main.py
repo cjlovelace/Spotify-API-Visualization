956a1b7e89b3f50be1d86e84058b65f609c95306
cjlovelace <cjlovela@umich.edu>
2022-04-12 13:37:08 -0400

    Uploaded user data for later data visualization. Some python changes for general syntax testing.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ eel.init("static_web_folder")
 def print_albums():
     auth_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(auth_manager=auth_manager)
+    sp2 = auth_manager.
 
     tbirdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 
@@ -16,12 +17,16 @@ def print_albums():
     albums = results['items']
     holder = ""
 
-    while results['next']:
-        results = sp.next(results)
-        albums.extend(results['items'])
+    userID = sp.user("12178010763")
+    user = sp.user
+    songs = user['items']
 
-    for album in albums:
-        holder += album['name']
+    while user['next']:
+        results = sp.next(user)
+        user.extend(songs['items'])
+
+    for songList in songs:
+        holder += songs['name']
         print(holder)
 
     return holder
